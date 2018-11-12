63f06e9545379db63f770994c9f03b0918c181b0
victor.li <victor.li@cssinco.cn>
2018-11-12 17:35:27 +0800

    fix bug

--- app/src/main/java/cn/innovativest/ath/ui/act/ShareAct.java
+++ app/src/main/java/cn/innovativest/ath/ui/act/ShareAct.java
@@ -481,12 +481,13 @@ public class ShareAct extends BaseAct implements AdapterView.OnItemClickListener
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
 
+
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
-        oks.setTitle("ATH分享");
+//        oks.setTitle("ATH分享");
         // titleUrl是标题的网络链接，QQ和QQ空间等使用
-        oks.setTitleUrl("http://ath.pub/");
+//        oks.setTitleUrl("http://ath.pub/");
         // text是分享文本，所有平台都需要这个字段
-        oks.setText(shareTxt);
+//        oks.setText(shareTxt);
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 //        oks.setImagePath(imgUrl);//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
@@ -495,11 +496,15 @@ public class ShareAct extends BaseAct implements AdapterView.OnItemClickListener
         } else {
             oks.setImageUrl(urlImg);
         }
-        oks.setUrl("http://ath.pub/");
+//        oks.setUrl("http://ath.pub/");
         // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
+//        oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://ath.pub/");
+//        oks.setSiteUrl("http://ath.pub/");
+
+        if (!TextUtils.isEmpty(shareTxt)) {
+            copy(shareTxt);
+        }
 
 // 启动分享GUI
         oks.show(this);
